5eac225659a9184eaa74fccc6d6772747650108c
ma9da <dodymasry25@gmail.com>
2024-09-16 00:10:05 +0300

    fixing configuration versions

---
+++ src/polyfills.ts
@@ -0,0 +1,5 @@
+// Zone JS is required by Angular itself.
+import 'zone.js'; // Included with Angular CLI.
+
+// Load `$localize` for Angular i18n support if needed.
+import '@angular/localize/init';
